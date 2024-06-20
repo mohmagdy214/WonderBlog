20eee5bfd021e7d84287b36c6ce48df4479fc6d7
Mahmoud Magdy <mohmagdy214@gmail.com>
2024-06-20 14:50:25 +0300

    add the models to the admin panel --- post & comment

--- DJblog/models.py
+++ DJblog/models.py
@@ -5,17 +5,24 @@ from taggit.managers import TaggableManager
 
 # Create your models here.
 
-class Post (models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
-    created_at = models.DateTimeField(timezone.now())
-    writer = models.ForeignKey(User,related_name='post_writer',on_delete=models.SET_NULL,null=True,blank=True)
-    tags = TaggableManager()
     content = models.TextField(max_length=20000)
+    writer = models.ForeignKey(User,related_name='post_writer',on_delete=models.SET_NULL,null=True,blank=True)
+    drafted = models.BooleanField(default=True)
+    tags = TaggableManager()
+    created_at = models.DateTimeField(timezone.now())
     image = models.ImageField(upload_to="posts_images")
 
+    def __str__(self):
+        return self.title
+    
 
-
-class Comment (models.Model):
+class Comment(models.Model):
+    comment = models.CharField(max_length=200)
     post = models.ForeignKey(Post,related_name='comment_post',on_delete=models.CASCADE)
     writer = models.ForeignKey(User,related_name='comment_writer',on_delete=models.CASCADE)
     created_at = models.DateTimeField(timezone.now())
+
+    def __str__(self):
+        return self.comment
