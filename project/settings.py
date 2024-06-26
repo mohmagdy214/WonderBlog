ff46f522e23d7aed4e4d1d1affde5f2fd98f5ca0
Mahmoud Magdy <mohmagdy214@gmail.com>
2024-06-26 20:31:03 +0300

    add bootstrap to the template pages

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DJblog',
     'taggit',
+    "django_bootstrap5",
+    'DJblog',
 ]
 
 MIDDLEWARE = [
